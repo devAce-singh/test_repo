4da79c4613b020a1220c961a539860d1d8f1516d
Devesh Singh2 <Devesh.Singh02@motherson.com>
2024-09-09 01:38:27 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -3,8 +3,10 @@ var app = builder.Build();
 
 app.MapPost("/", async webhookPayload => {
     //var requestBody = await context.Request.ReadFromJsonAsync<WebhookPayload>();
+    Console.WriteLine("Webhook Call is Successfull!");
     Console.WriteLine("webhookPayload : " + webhookPayload);
-    //Console.WriteLine($"Header: {requestBody?.Header}, Body: {requestBody?.Body}");
+    Console.WriteLine("webhookPayload Request : " + webhookPayload.Request);
+    Console.WriteLine($"webhookPayload Request Header: {webhookPayload.Request.Headers}, webhookPayload Request Body: {webhookPayload.Request.Body}");
 
     webhookPayload.Response.StatusCode = 200;
     await webhookPayload.Response.WriteAsync("Webhook Calling Successfully!");
